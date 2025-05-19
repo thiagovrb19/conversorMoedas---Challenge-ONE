7520121d5d8a591baa823540b93d1de5c2b6580d
Thiago <132407593+thiagovrb19@users.noreply.github.com>
2025-05-19 10:19:12 -0300

    Delete converter directory

--- converter/src/main/java/com/thiago/ui/Converter.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.thiago.ui;
-
-public class Converter {
-    public static double convert(double amount, double rateBase, double rateTarget) {
-        double amountInUsd = amount / rateBase;
-        return amountInUsd * rateTarget;
-    }
-}
